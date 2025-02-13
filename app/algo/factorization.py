5ea53c5843ab2a44ecd803ce5b3b18095838314d
TR0205 <index0205@gmail.com>
2025-02-13 20:49:10 +0000

    update

--- app/algo/factorization.py
+++ app/algo/factorization.py
@@ -9,49 +9,16 @@ cat input.txt | python factorization.py
 
 # TODO: 間違っているので直す
 
-n = int(input())
+# n = int(input())
+n = 64
 
 r = []
-while n % 2 == 0:
-    # print('n: {n}'.format(n=int(n)))
-    
-    r.append(2)
-    n /= 2
+for i in range(2, int(n**0.5)+1):
+    while n % i == 0:
+        r.append(i)
+        n /= i
+if n > 1:
+    r.append(n)
 
-# print('finished divied 2. n: {n}'.format(n=int(n)))
-# print(r)
-    
-while n % 3 == 0:
-    # print('n: {n}'.format(n=int(n)))
-
-    r.append(3)
-    n /= 3
-
-# print('finished divied 3. n: {n}'.format(n=int(n)))
-# print(r)
-
-while n % 5 == 0:
-    # print('n: {n}'.format(n=int(n)))
-
-    r.append(5)
-    n /= 5
-
-# print('finished divied 5. n: {n}'.format(n=int(n)))
-# print(r)
-
-if n != 1:
-    r.append(int(n))
-
-# print('finished divied ALL. n: {n}'.format(n=int(n)))
-# print(r)
-
-# map()でリストの各要素をstringへ変換
-# intのままだとjoin()で使用できない
-str_list = list(map(str, r))
-res = ' '.join(str_list)
-print(res)
-
-l = [1, 2, 3]
-str_list = map(str, l)
-res = ' '.join(str_list)
-print(res)
+r_str = map(str, r)
+print(' '.join(r_str))
